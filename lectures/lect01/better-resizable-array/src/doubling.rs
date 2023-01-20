40b000a565b8047223466bc4e5d27bae01554fc9
nawat <n.ngerncham@gmail.com>
2023-01-21 00:51:11 +0700

    implemented some logic for hat

--- lectures/lect01/better-resizable-array/src/doubling.rs
+++ lectures/lect01/better-resizable-array/src/doubling.rs
@@ -1,6 +1,6 @@
 use crate::resizable_arr::ResizableArray;
 
-const DEFAULT_INIT_CAP: usize = 8;
+const DEFAULT_INIT_CAP: usize = 16;
 
 pub struct DoublingResizableArray<T> {
     cap: usize,
@@ -20,6 +20,10 @@ impl<T> DoublingResizableArray<T> {
 
 impl<T> ResizableArray<T> for DoublingResizableArray<T> where T: Copy {
     fn push_back(&mut self, e: T) {
+        if self.n == self.cap {
+            self.expand();
+        }
+
         self.arr.push(e);
     }
 
@@ -34,6 +38,7 @@ impl<T> ResizableArray<T> for DoublingResizableArray<T> where T: Copy {
         if self.n < self.cap / 4 {
             self.shrink();
         }
+
         return self.arr.pop().unwrap();
     }
 
--- lectures/lect01/better-resizable-array/src/hat.rs
+++ lectures/lect01/better-resizable-array/src/hat.rs
@@ -1,4 +1,6 @@
-const DEFAULT_INIT_K: usize = 3;
+use crate::resizable_arr::ResizableArray;
+
+const DEFAULT_INIT_K: usize = 4;
 
 pub struct HATResizableArray<T> {
     k: usize, // for B = 2^k
@@ -17,4 +19,55 @@ impl<T> HATResizableArray<T> {
             top: v,
         }
     }
+}
+
+impl<T> ResizableArray<T> for HATResizableArray<T> where T: Copy {
+    fn push_back(&mut self, e: T) {
+        if self.n >= 2 >> self.k {
+            self.expand();
+        }
+
+        let top_index = compute_top_idx(self.n, self.k);
+        self.top[top_index].push(e);
+    }
+
+    fn expand(&mut self) {
+        self.k = self.k * 2;
+
+        let mut new_top: Vec<Vec<T>> = Vec::with_capacity(self.k);
+        for _ in 0..self.k / 2 {
+            new_top.push(Vec::with_capacity(self.k));
+        }
+        
+    }
+
+    fn pop_back(&mut self) -> T {
+        let i = self.n - 1;
+
+        let top_index = compute_top_idx(i, self.k);
+        let leaf_index = compute_leaf_idx(i, self.k);
+
+        let leaf = &self.top[top_index];
+        return leaf[leaf_index];
+    }
+
+    fn shrink(&mut self) {
+        
+    }
+
+    fn get(&self, i: usize) -> T {
+        let top_index = compute_top_idx(i, self.k);
+        let leaf_index = compute_leaf_idx(i, self.k);
+
+        let leaf = &self.top[top_index];
+        return leaf[leaf_index];
+    }
+}
+
+fn compute_top_idx(i: usize, k: usize) -> usize {
+    return i >> k;
+}
+
+fn compute_leaf_idx(i: usize, k: usize) -> usize {
+    return i & (1 << k - 1);
 }
